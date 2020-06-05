8edd1ba941f92b34a57672a2d2ae79261a2cfdad
Caraka Putra <carakaputramega@gmail.com>
2020-06-05 09:49:35 +0700

    tag seeder

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -18,9 +18,9 @@ class TagsTableSeeder extends Seeder
         $users = Role::findOrFail(2)->users;
 
         $tags = [
-            ['name' => 'Milagros Malang','address' => 'Perum Griyashanta Blk. B No.116 Mojolangu','latitude' => '-7.939794','longitude' => '112.621231','description' => 'Restoran','img' => '#','active' => '1','created_at' => Carbon::now()],
-            ['name' => 'SMP Negeri 18 Malang','address' => 'Jalan Soekarno Hatta No. A-394, Lowokwaru','latitude' => '-7.939397','longitude' => '112.619537','description' => 'Bar','img' => '#','active' => '1','created_at' => Carbon::now()],
-            ['name' => 'Taman Krida Budaya','address' => 'Jl. Soekarno Hatta No.7 Jatimulyo','latitude' => '-7.942616','longitude' => '112.622520','description' => 'Bar','img' => '#','active' => '1','created_at' => Carbon::now()],
+            ['name' => 'Milagros Malang','address' => 'Perum Griyashanta Blk. B No.116 Mojolangu','latitude' => '-7.939794','longitude' => '112.621231','description' => 'Restoran','img' => 'assets/images/tags/milagros.png','active' => '1','created_at' => Carbon::now()],
+            ['name' => 'SMP Negeri 18 Malang','address' => 'Jalan Soekarno Hatta No. A-394, Lowokwaru','latitude' => '-7.939397','longitude' => '112.619537','description' => 'Bar','img' => 'assets/images/tags/smpn8mlg.png','active' => '1','created_at' => Carbon::now()],
+            ['name' => 'Taman Krida Budaya','address' => 'Jl. Soekarno Hatta No.7 Jatimulyo','latitude' => '-7.942616','longitude' => '112.622520','description' => 'Bar','img' => 'assets/images/tags/tamankrida.png','active' => '1','created_at' => Carbon::now()],
             
         ];
         
